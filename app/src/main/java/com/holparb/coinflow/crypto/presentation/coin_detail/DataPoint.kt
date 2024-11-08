1f864ac43f44674493a524f5622cec134539d43c
Balazs Holpar <balazs02.holpar@gmail.com>
2024-11-08 08:59:13 +0100

    Draw viewport and x/y labels

--- app/src/main/java/com/holparb/coinflow/crypto/presentation/coin_detail/DataPoint.kt
+++ app/src/main/java/com/holparb/coinflow/crypto/presentation/coin_detail/DataPoint.kt
@@ -3,6 +3,5 @@ package com.holparb.coinflow.crypto.presentation.coin_detail
 data class DataPoint(
     val x: Float,
     val y: Float,
-    val xLabel: String,
-    val yLabel: ValueLabel
+    val xLabel: String
 )
--- app/src/main/java/com/holparb/coinflow/crypto/presentation/coin_detail/components/LineChart.kt
+++ app/src/main/java/com/holparb/coinflow/crypto/presentation/coin_detail/components/LineChart.kt
@@ -3,8 +3,9 @@ package com.holparb.coinflow.crypto.presentation.coin_detail.components
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.LocalTextStyle
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -13,11 +14,23 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.drawText
 import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.holparb.coinflow.crypto.domain.model.CoinPrice
 import com.holparb.coinflow.crypto.presentation.coin_detail.ChartStyle
 import com.holparb.coinflow.crypto.presentation.coin_detail.DataPoint
+import com.holparb.coinflow.crypto.presentation.coin_detail.ValueLabel
 import com.holparb.coinflow.ui.theme.CoinFlowTheme
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import kotlin.random.Random
 
 @Composable
 fun LineChart(
@@ -42,13 +55,14 @@ fun LineChart(
     val maxYValue = remember(visibleDataPoints) {
         visibleDataPoints.maxOfOrNull { it.y } ?: 0f
     }
-    val maxXValue = remember(visibleDataPoints) {
-        visibleDataPoints.maxOfOrNull { it.x } ?: 0f
+
+    val minYValue = remember(visibleDataPoints) {
+        visibleDataPoints.minOfOrNull { it.y } ?: 0f
     }
 
     val measurer = rememberTextMeasurer()
 
-    val xLabelWidth by remember {
+    var xLabelWidth by remember {
         mutableFloatStateOf(0f)
     }
     LaunchedEffect(key1 = xLabelWidth) {
@@ -68,24 +82,135 @@ fun LineChart(
     Canvas(
         modifier = modifier.fillMaxSize()
     ) {
+        val minLabelSpacingYPx = style.minYLabelSpacing.toPx()
+        val verticalPaddingInPx = style.verticalPadding.toPx()
+        val horizontalPaddingPx = style.horizontalPadding.toPx()
+        val xAxisLabelSpacingPx = style.xAxisLabelSpacing.toPx()
 
+        val xLabelTextLayoutResults = visibleDataPoints.map {
+            measurer.measure(
+                text = it.xLabel,
+                style = textStyle.copy(textAlign = TextAlign.Center)
+            )
+        }
+        val maxXLabelWidth = xLabelTextLayoutResults.maxOfOrNull { it.size.width } ?: 0
+        val maxXLabelHeight = xLabelTextLayoutResults.maxOfOrNull { it.size.height } ?: 0
+        val maxXLabelLineCount = xLabelTextLayoutResults.maxOfOrNull { it.lineCount } ?: 0
+        val xLabelLineHeight = maxXLabelHeight / maxXLabelLineCount
+
+        val viewPortHeightPx = size.height -
+                (maxXLabelHeight + 2 * verticalPaddingInPx
+                        + xLabelLineHeight + xAxisLabelSpacingPx)
+
+        val viewPortTopY = verticalPaddingInPx + xLabelLineHeight + 10f
+        val viewPortRightX = size.width
+        val viewPortBottomY = viewPortTopY + viewPortHeightPx
+        val viewPortLeftX = 2f * horizontalPaddingPx
+        val viewPort = Rect(
+            left = viewPortLeftX,
+            right = viewPortRightX,
+            top = viewPortTopY,
+            bottom = viewPortBottomY
+        )
+
+        drawRect(
+            color = Color.Green,
+            topLeft = viewPort.topLeft,
+            size = viewPort.size
+        )
+
+        xLabelWidth = maxXLabelWidth + xAxisLabelSpacingPx
+        xLabelTextLayoutResults.forEachIndexed { index, result ->
+            drawText(
+                textLayoutResult = result,
+                topLeft = Offset(
+                    x = viewPortLeftX + xAxisLabelSpacingPx / 2f + xLabelWidth * index,
+                    y = viewPortBottomY + xAxisLabelSpacingPx
+                )
+            )
+        }
+
+        val labelViewPortHeightPx = viewPortHeightPx + xLabelLineHeight
+        val labelCountExcludingLastLabel = ((labelViewPortHeightPx / (xLabelLineHeight + minLabelSpacingYPx))).toInt()
+
+        val valueIncrement = (maxYValue - minYValue) / labelCountExcludingLastLabel
+
+        val yLabels = (0 .. labelCountExcludingLastLabel).map { index ->
+            ValueLabel(
+                value = maxYValue - (valueIncrement * index),
+                unit = unit
+            )
+        }
+
+        val yLabelTextLayoutResults = yLabels.map {
+            measurer.measure(
+                text = it.formatted(),
+                style = textStyle
+            )
+        }
+
+        val heightRequiredForLabels = xLabelLineHeight * (labelCountExcludingLastLabel + 1)
+        val remainingHeightForLabels = viewPortHeightPx - heightRequiredForLabels
+        val spaceBetweenLabels = remainingHeightForLabels / labelCountExcludingLastLabel
+        val maxYLabelWidth = yLabelTextLayoutResults.maxOfOrNull { it.size.width } ?: 0
+        yLabelTextLayoutResults.forEachIndexed { index, result  ->
+            drawText(
+                textLayoutResult = result,
+                topLeft = Offset(
+                    x = horizontalPaddingPx + maxYLabelWidth - result.size.width.toFloat(),
+                    y = viewPortTopY + index * (xLabelLineHeight + spaceBetweenLabels) - xLabelLineHeight / 2f
+                )
+            )
+        }
     }
 }
 
-@Preview
+@Preview(widthDp = 1000)
 @Composable
 private fun LineChartPreview() {
     CoinFlowTheme {
-        /*
-        LineChart(
-            dataPoints = listOf(),
-            style = ChartStyle(
-                chartLineColor =
-            ),
-            visibleDataPointsIndices = 1..20,
-            unit = "$",
-            modifier = Modifier.background(MaterialTheme.colorScheme.background)
+        val coinHistoryRandomized = remember {
+            (1 .. 20).map {
+                CoinPrice(
+                    priceUsd = Random.nextFloat() * 1000.0,
+                    dateTime = ZonedDateTime.now().plusHours(it.toLong())
+                )
+            }
+        }
+        val style = ChartStyle(
+            chartLineColor = Color.Black,
+            unselectedColor = Color(0xFF7C7C7C),
+            selectedColor = Color.Black,
+            helperLineThicknessPx = 1f,
+            axisLinesThicknessPx = 5f,
+            labelFontSize = 14.sp,
+            minYLabelSpacing = 25.dp,
+            verticalPadding = 8.dp,
+            horizontalPadding = 8.dp,
+            xAxisLabelSpacing = 8.dp
         )
-        */
+        val dataPoints = remember {
+            coinHistoryRandomized.map {
+                DataPoint(
+                    x = it.dateTime.hour.toFloat(),
+                    y = it.priceUsd.toFloat(),
+                    xLabel = DateTimeFormatter
+                        .ofPattern("ha\nM/d")
+                        .format(it.dateTime)
+                )
+            }
+        }
+
+        LineChart(
+            dataPoints = dataPoints,
+            style = style,
+            visibleDataPointsIndices = 1..19,
+            unit = "$",
+            modifier = Modifier
+                .width(700.dp)
+                .height(300.dp)
+                .background(Color.White),
+            selectedDataPoint = dataPoints[1]
+        )
     }
 }
