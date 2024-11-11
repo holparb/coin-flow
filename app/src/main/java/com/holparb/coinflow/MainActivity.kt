4b3a2c73848ef99e735c2e12ccd04490095ee443
Balazs Holpar <balazs02.holpar@gmail.com>
2024-11-11 13:57:09 +0100

    Add adaptive navigation for list and detail screens

--- app/src/main/java/com/holparb/coinflow/MainActivity.kt
+++ app/src/main/java/com/holparb/coinflow/MainActivity.kt
@@ -1,7 +1,6 @@
  package com.holparb.coinflow
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -10,13 +9,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.holparb.coinflow.core.presentation.util.ObserveAsEvents
-import com.holparb.coinflow.core.presentation.util.toString
-import com.holparb.coinflow.crypto.presentation.coin_detail.CoinDetailScreen
-import com.holparb.coinflow.crypto.presentation.coin_list.CoinListEvent
-import com.holparb.coinflow.crypto.presentation.coin_list.CoinListScreen
+import com.holparb.coinflow.core.navigation.AdaptiveCoinListDetailPane
 import com.holparb.coinflow.crypto.presentation.coin_list.CoinListViewModel
 import com.holparb.coinflow.ui.theme.CoinFlowTheme
 import org.koin.androidx.compose.koinViewModel
@@ -30,33 +24,10 @@ import org.koin.androidx.compose.koinViewModel
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     val viewModel = koinViewModel<CoinListViewModel>()
                     val state by viewModel.state.collectAsStateWithLifecycle()
-                    val context = LocalContext.current
-                    ObserveAsEvents(events = viewModel.events) { event ->
-                        when(event) {
-                            is CoinListEvent.Error -> {
-                                Toast.makeText(
-                                    context,
-                                    event.error.toString(context),
-                                    Toast.LENGTH_LONG
-                                ).show()
-                            }
-                        }
-                    }
-                    when {
-                        state.selectedCoin != null -> {
-                            CoinDetailScreen(
-                                state = state,
-                                modifier = Modifier.padding(innerPadding)
-                            )
-                        }
-                        else -> {
-                            CoinListScreen(
-                                state = state,
-                                modifier = Modifier.padding(innerPadding),
-                                onAction = viewModel::onAction
-                            )
-                        }
-                    }
+
+                    AdaptiveCoinListDetailPane(
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
